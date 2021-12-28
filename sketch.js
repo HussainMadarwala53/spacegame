7b21ac5015fc11025e33dd7d04f31cb8c2f96ada
HussainMadarwala53 <70691828+HussainMadarwala53@users.noreply.github.com>
2021-12-28 21:52:02 +0530

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -1,14 +1,16 @@
-var bg,canvas;
+var bg, canvas;
 
-function preload(){
-    bg=loadImage("download.png")
- 
+function preload() {
+  backImage = loadImage("download.png");
 }
 
-function setup(){
-    var canvas=createCanvas(1200,600);
+function setup() {
+  var canvas = createCanvas(1200, 600);
+  backgr = createSprite(0, 0, 800, 400);
+  backgr.addImage(backImage);
+  backgr.scale = 1.5;
 }
-function draw(){
-    background(bg);
-    drawSprites();
+function draw() {
+  // background(blue);
+  drawSprites();
 }
